80592a8ec6ef95a3178e34f113dd60c8be6cfb0f
darwinl-06 <darwinlenis.06@gmail.com>
2025-04-30 08:05:23 -0500

    cambio

--- main.tf
+++ main.tf
@@ -13,7 +13,16 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  skip_provider_registration = true
 }
 
 provider "random" {}
